4100f890b4939777908aefda5412317fb8d997e7
Đỗ Tôn <67794492+thanhtonvk@users.noreply.github.com>
2022-03-01 06:02:59 +0700

    .

---
+++ Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace BaiTH2.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
